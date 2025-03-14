514cfbf6a9c67e6d3f46ef7ff08cd213d1095352
Lucar-aster <luca.r@astercucine.it>
2025-03-14 15:40:22 +0100

    Create timeline.js

---
+++ timeline.js
@@ -0,0 +1,15 @@
+var timeline = null;
+
+function renderTimeline() {
+    var container = document.getElementById('timeline');
+    var options = {
+        stack: true,
+        editable: true,
+        margin: { item: 10 },
+        showCurrentTime: true,
+        zoomable: true,
+        locale: 'it'
+    };
+
+    timeline = new vis.Timeline(container, tasks, groups, options);
+}
