7a1eaf87976ed5d2b6c67fd3372d8e599224bd79
Lucar-aster <luca.r@astercucine.it>
2025-03-14 15:39:02 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,10 @@
+var groups = [
+    { id: 1, content: 'Gruppo A' },
+    { id: 2, content: 'Gruppo B' },
+    { id: 3, content: 'Gruppo C' }
+];
+
+var tasks = [
+    { id: 1, content: 'Task 1', start: '2023-03-10T10:00:00', end: '2023-03-10T12:00:00', group: 1, style: "background-color: #ffcc00;" },
+    { id: 2, content: 'Task 2', start: '2023-03-10T14:00:00', end: '2023-03-10T16:00:00', group: 1, style: "background-color: #00ccff;" }
+];
