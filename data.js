a7f59ca240e16be55b3038910ca5b274dec06066
Lucar-aster <luca.r@astercucine.it>
2025-03-14 15:44:05 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,10 +1,13 @@
-var groups = [
-    { id: 1, content: 'Gruppo A' },
-    { id: 2, content: 'Gruppo B' },
-    { id: 3, content: 'Gruppo C' }
-];
+     var groups = [
+            {id: 1, content: 'Gruppo A'},
+            {id: 2, content: 'Gruppo B'},
+            {id: 3, content: 'Gruppo C'}
+        ];
 
-var tasks = [
-    { id: 1, content: 'Task 1', start: '2023-03-10T10:00:00', end: '2023-03-10T12:00:00', group: 1, style: "background-color: #ffcc00;" },
-    { id: 2, content: 'Task 2', start: '2023-03-10T14:00:00', end: '2023-03-10T16:00:00', group: 1, style: "background-color: #00ccff;" }
-];
+        var tasks = [
+            { id: 1, content: 'Task 1', start: '2023-03-10T10:00:00', end: '2023-03-10T12:00:00', group: 1, style: "background-color: #ffcc00;" },
+            { id: 2, content: 'Task 2', start: '2023-03-10T14:00:00', end: '2023-03-10T16:00:00', group: 1, style: "background-color: #00ccff;" },
+            { id: 3, content: 'Task 3', start: '2023-03-11T09:00:00', end: '2023-03-11T11:00:00', group: 2, style: "background-color: #ff6666;" },
+            { id: 4, content: 'Task 4', start: '2023-03-12T09:00:00', end: '2023-03-12T10:00:00', group: 2, style: "background-color: #66ff66;" },
+            { id: 5, content: 'Task 5', start: '2023-03-13T14:00:00', end: '2023-03-13T15:00:00', group: 3, style: "background-color: #9966ff;" }
+        ];
