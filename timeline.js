3040504c6e887e90192a01139b6fffe3b0544461
Lucar-aster <luca.r@astercucine.it>
2025-03-17 09:18:27 +0100

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -9,10 +9,9 @@ function renderTimeline() {
         groupEditable: true
         margin: { item: 10 },
         showCurrentTime: true,
-        showWeekScale: false
         zoomable: true,
         locale: 'it'
-    {
+    
     timeAxis: {
       scale: 'day',
       step: 1
@@ -22,7 +21,7 @@ function renderTimeline() {
       minorLabels: {day: 'DD/MMM'},
       majorLabels: {day: 'w'}
     }
-  }
+  
     };
 
     timeline = new vis.Timeline(container, tasks, groups, options);
